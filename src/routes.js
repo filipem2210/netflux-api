24de1cddbfd3040c792ba9b5880f45f8ebc516cb
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-05-23 21:06:42 -0300

    add validation with celebrate on signup route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const UserController = require('./app/controllers/UserController');
 
 const routes = Router();
 
-routes.post('/users', UserController.store);
+routes.post(
+  '/signup',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).max(10).required(),
+    }),
+  }),
+  UserController.store,
+);
 
 module.exports = routes;
