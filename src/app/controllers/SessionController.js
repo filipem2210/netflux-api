1f8532334b7ecf62395e3b896c3e5ddff6c1dc75
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-05-23 21:55:27 -0300

    refactor session controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -8,15 +8,19 @@ module.exports = {
       const { email, password } = req.body;
 
       const user = await User.findOne({ where: { email: req.body.email } });
+
       if (!user) return res.status(401).json({ error: 'User not found' });
 
       const passwordMatch = await checkPassword.compare(password, user.password);
+
       if (!passwordMatch) {
         return res.status(401).json({ error: 'Password does not match' });
       }
 
       const {
-        id, name, avatar,
+        id,
+        name,
+        avatar,
       } = user;
 
       const token = await generateToken(id);
