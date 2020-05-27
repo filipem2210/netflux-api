184feb4b7f4468cd32eb8b15b8e2ab3868980f90
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-05-27 19:41:02 -0300

    remove deprecated sequelize config

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,6 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  operatorAliases: false,
   logging: false,
   define: {
     timestamps: true,
