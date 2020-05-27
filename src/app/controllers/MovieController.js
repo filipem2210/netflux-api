7f4895f65f7770095da373009c1b3aca9dc8db7e
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-05-27 19:25:06 -0300

    update redis config on moviecontroller

--- src/app/controllers/MovieController.js
+++ src/app/controllers/MovieController.js
@@ -2,11 +2,9 @@ require('dotenv').config();
 const redis = require('redis');
 const { Movie } = require('../models');
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-
 const redisClient = redis.createClient({
-  host: 'redis',
-  port: REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
 module.exports = {
